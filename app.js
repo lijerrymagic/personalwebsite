d6d694fffc78f0f46a6ffd9e1f61e4cc8b2a9800
li_jerry <lijerrymagic@gmail.com>
2018-08-15 21:35:12 -0400

    zeyuli 1.0

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+
+app.use(function (req, res, next){
+    console.log("HTTP request", req.method, req.url, req.body);
+    return next();
+});
+
+// Constants
+const PORT = 3000;
+const HOST = '0.0.0.0';
+
+app.get('/', (req, res) => res.redirect('/index.html'))
+
+app.use(express.static('frontend'));
+
+var http = require('http');
+
+http.createServer(app).listen(process.env.PORT, function () {
+    console.log('HTTP on port');
+});
+// http.createServer(app).listen(PORT, function () {
+//     console.log('HTTP on port 3000');
+// });
