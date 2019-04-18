6f1f1629317f65102d1cae547e22c91401154b68
li_jerry <lijerrymagic@gmail.com>
2019-04-17 22:32:08 -0400

    update app to redirect http tp https

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require('express')
 const fs = require('fs')
 const https = require('https');
+const http = require('http');
 const app = express()
 
 app.use(function (req, res, next){
-    console.log("HTTPS request", req.method, req.url, req.body);
-    return next();
+	if (req.protocol === 'http'){
+		res.redirect(301, `https://${req.headers.host}${req.url}`)
+		console.log("HTTPS request", req.method, req.url, req.body);
+    	return next();
+	}
 });
 
 // Constants
@@ -13,9 +17,9 @@ app.use(function (req, res, next){
 // const HOST = '0.0.0.0';
 
 let httpsOptions = {
-   cert:fs.readFileSync('./ssl/www_zeyuli_me.crt');
-   ca:fs.readFileSync('./ssl/www_zeyuli_me.ca-bundle');
-   key:fs.readFileSync('./ssl/zeyuli_me.key');
+   cert:fs.readFileSync('./ssl/www_zeyuli_me.crt'),
+   ca:fs.readFileSync('./ssl/www_zeyuli_me.ca-bundle'),
+   key:fs.readFileSync('./ssl/zeyuli_me.key')
 };
 // app.get('/', (req, res) => res.redirect('/index.html'))
 
@@ -23,6 +27,9 @@ app.use(express.static('frontend'));
 
 https.createServer(httpsOptions,app).listen(process.env.PORT, function () {
     console.log('HTTPS on port');
+});
+http.createServer(app).listen(process.env.PORT, function () {
+    console.log('HTTP on port');
 });
 // http.createServer(app).listen(PORT, function () {
 //     console.log('HTTP on port 3000');
