0e3203fd1eea1826b4cba6db0d2904c7ef381b4c
li_jerry <lijerrymagic@gmail.com>
2019-04-17 22:06:11 -0400

    Update app.js add ssl certificate
    Now my website support HTTPS

--- app.js
+++ app.js
@@ -1,23 +1,28 @@
 const express = require('express')
+const fs = require('fs')
+const https = require('https');
 const app = express()
 
 app.use(function (req, res, next){
-    console.log("HTTP request", req.method, req.url, req.body);
+    console.log("HTTPS request", req.method, req.url, req.body);
     return next();
 });
 
 // Constants
-const PORT = 3000;
-const HOST = '0.0.0.0';
+// const PORT = 3000;
+// const HOST = '0.0.0.0';
 
+let httpsOptions = {
+   cert:fs.readFileSync('./ssl/www_zeyuli_me.crt');
+   ca:fs.readFileSync('./ssl/www_zeyuli_me.ca-bundle');
+   key:fs.readFileSync('./ssl/zeyuli_me.key');
+};
 // app.get('/', (req, res) => res.redirect('/index.html'))
 
 app.use(express.static('frontend'));
 
-var http = require('http');
-
-http.createServer(app).listen(process.env.PORT, function () {
-    console.log('HTTP on port');
+https.createServer(httpsOptions,app).listen(process.env.PORT, function () {
+    console.log('HTTPS on port');
 });
 // http.createServer(app).listen(PORT, function () {
 //     console.log('HTTP on port 3000');
